3f0221e7f3b60ab788357aee6faf446bd3b2b2d0
Kamil Zasada <pankamilz@gmail.com>
2016-02-24 20:52:35 +0100

    Add -f option to read RAML file from file system.

--- ramlo.js
+++ ramlo.js
@@ -7,33 +7,29 @@ var program = require('commander');
 var raml = require('raml-1-parser');
 
 var fs = require('fs');
-var fName = path.resolve(__dirname, 'test.raml');
 
-var api = raml.loadApiSync(fName);
-//var apiResources = api.resources();
-var apiResources = api.allResources();
+program
+    .version(pkg.version)
+    .option('-f, --file [path]', 'RAML file')
+    .parse(process.argv);
 
-apiResources.forEach(function(resource) {
-    // console.log(resource.displayName());
-    console.log(resource.kind() + ' : ' + resource.absoluteUri());
+if (program.file) {
+    var fName = path.resolve(process.cwd(), program.file);
+    var api = raml.loadApiSync(fName);
+    var apiResources = api.allResources();
 
-    resource.methods().forEach(function(method) {
-        console.log('\t' + method.method());
+    apiResources.forEach(function(resource) {
+        // console.log(resource.displayName());
+        console.log(resource.kind() + ' : ' + resource.absoluteUri());
 
-        method.responses().forEach(function(response) {
-            console.log('\t\t' + response.code().value());
+        resource.methods().forEach(function(method) {
+            console.log('\t' + method.method());
+
+            method.responses().forEach(function(response) {
+                console.log('\t\t' + response.code().value());
+            });
         });
     });
-});
 
-
-//console.log('ramlo');
-//console.log(JSON.stringify(api.toJSON(), null, 2));
-
-
-// program
-//     .version(pkg.version)
-//     .option('-p, --port <port>', 'Port on which to listen to (defaults to 3000)', parseInt)
-//     .parse(process.argv);
-//
-// var port = program.port || 3000;
+     // console.log(JSON.stringify(api.toJSON(), null, 2));
+}
