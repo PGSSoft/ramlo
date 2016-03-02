8e20c43f8994b3610544e86a415ba5ae80c46744
Kamil Zasada <pankamilz@gmail.com>
2016-03-03 00:07:55 +0100

    Continue work on documentation template. Added list of resources, endpoints and query parameters.

--- ramlo.js
+++ ramlo.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
 var path = require('path');
-var pkg = require(path.join(__dirname, 'package.json'));
-
+var fs = require('fs');
 var program = require('commander');
 var raml = require('raml-1-parser');
-
-var fs = require('fs');
-
 var sass = require('node-sass');
 var jade = require('jade');
+var pkg = require(path.join(__dirname, 'package.json'));
 
 program
     .version(pkg.version)
@@ -19,9 +16,39 @@ program
 if (program.file) {
     var fName = path.resolve(process.cwd(), program.file);
     var api = raml.loadApiSync(fName);
+
+    var apiResources = [];
+    var resources = api.resources();
+
+    resources.forEach(function(resource) {
+        var apiMethods = [];
+
+        resource.methods().forEach(function(method) {
+            var queryParameters = [];
+
+            method.queryParameters().forEach(function(parameter) {
+                queryParameters.push({
+                    name: parameter.name(),
+                    type: parameter.type()
+                });
+            });
+
+            apiMethods.push({
+                name: method.method(),
+                parameters: queryParameters
+            });
+        });
+
+        apiResources.push({
+            name: resource.displayName(),
+            uri: resource.completeRelativeUri(),
+            methods: apiMethods
+        });
+    });
+
+    /**
     var apiResources = api.allResources();
     var resources = [];
-
     apiResources.forEach(function(resource) {
         // console.log(resource.displayName());
         //console.log(resource.kind() + ' : ' + resource.absoluteUri());
@@ -35,24 +62,31 @@ if (program.file) {
             });
         });
     });
+    **/
 
+    // pass variables to jade template
     var locals = {
-        pageTitle: 'ramlo',
-        resources: resources
+        apiTitle: api.title(),
+        apiDescription: api.description().value(),
+        ramlVersion: api.RAMLVersion(),
+        apiResources: apiResources
     };
+    console.log(JSON.stringify(locals));
 
-    //console.log(locals);
-
+    // compile sass styles
     var scss = sass.renderSync({
         file: path.join(__dirname, 'src/main.scss'),
         outputStyle: 'compressed',
         outFile: path.join(__dirname, 'src/main.css'),
         sourceMap: false
     });
+
+    // save css file which will be included in html file
+    fs.writeFileSync(path.join(__dirname, 'src/main.css'), scss.css);
+
+    // render html from jade template
     var html = jade.renderFile(path.join(__dirname, 'src/index.jade'), locals);
 
-    fs.writeFile(path.join(__dirname, 'src/main.css'), scss.css);
-    fs.writeFile(path.resolve(process.cwd(), 'apidoc.html'), html);
-
-     // console.log(JSON.stringify(api.toJSON(), null, 2));
+    // save html file with documentation
+    fs.writeFileSync(path.resolve(process.cwd(), 'apidoc.html'), html);
 }
