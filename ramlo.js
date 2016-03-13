7453efdfe5f2dbd80ab96b68df68e8c6e6e63691
Kamil Zasada <pankamilz@gmail.com>
2016-03-13 12:57:51 +0100

    Code refactoring.

--- ramlo.js
+++ ramlo.js
@@ -2,142 +2,29 @@
 
 var path = require('path');
 var fs = require('fs');
+
 var program = require('commander');
-var raml = require('raml-1-parser');
+var chalk = require('chalk');
 var sass = require('node-sass');
 var jade = require('jade');
-var hljs = require('highlight.js');
-var _ = require('lodash');
 
 var pkg = require(path.join(__dirname, 'package.json'));
+var api = require('./src/modules/api');
 
 program
     .version(pkg.version)
-    .option('-f, --file [path]', 'RAML file')
+    .option('-f, --file <path>', 'RAML input file')
     .parse(process.argv);
 
-console.time('ramlo');
-
 if (program.file) {
-    var fName = path.resolve(process.cwd(), program.file);
-    var api = raml.loadApiSync(fName);
+    var docFile = 'api.html';
 
-    var apiDocumentations = [];
-    var documentations = api.documentation();
+    console.log(chalk.blue('starting ramlo...'));
+    console.time('time');
 
-    var apiResources = [];
-    var resources = api.resources();
-
-    // get documentations
-    // TODO: use Markdown
-    documentations.forEach(function(documentation) {
-        apiDocumentations.push({
-            title: documentation.title(),
-            content: documentation.content() && documentation.content().value()
-        });
-    });
-
-    // create list of resources (root level)
-    resources.forEach(function(resource) {
-        var endpoints = [];
-
-        // get methods of main resources
-        resource.methods().forEach(function(method) {
-            var example = '';
-
-            method.responses().forEach(function(response) {
-                response.body().forEach(function(body) {
-                    example = body.toJSON().example;
-                    example = example && hljs.highlight('json', example).value;
-                });
-            });
-
-            endpoints.push({
-                method: method.method(),
-                uri: resource.completeRelativeUri(),
-                description: method.description() && method.description().value(),
-                example: example
-            });
-        });
-
-        // get methods of resources at 2. level
-        // TODO: refactor this function to use recursion
-        resource.resources().forEach(function(resource) {
-            var uriParameters = [];
-            var queryParameters = [];
-
-            resource.uriParameters().forEach(function(parameter) {
-                uriParameters.push({
-                    name: parameter.name(),
-                    description: parameter.description() && parameter.description().value()
-                });
-            });
-
-            resource.methods().forEach(function(method) {
-                var example = '';
-                var schema = '';
-                var schemaObject;
-                var schemaProperties = [];
-
-                method.queryParameters().forEach(function(parameter) {
-                    queryParameters.push({
-                        name: parameter.name(),
-                        type: parameter.type(),
-                        description: parameter.description() && parameter.description().value()
-                    });
-                });
-
-                method.responses().forEach(function(response) {
-                    response.body().forEach(function(body) {
-                        schema = body.schemaContent();
-
-                        example = body.toJSON().example;
-                        example = example && hljs.highlight('json', example).value;
-
-                        schemaObject = JSON.parse(schema);
-
-                        if (schemaObject && schemaObject.properties) {
-                            _.forOwn(schemaObject.properties, function(value, key) {
-                                schemaProperties.push({
-                                    name: key,
-                                    type: value.type,
-                                    description: value.description,
-                                    isRequired: value.required
-                                });
-                            });
-                        }
-                    });
-                });
-
-                endpoints.push({
-                    method: method.method(),
-                    uri: resource.completeRelativeUri(),
-                    description: method.description() && method.description().value(),
-                    uriParameters: uriParameters,
-                    queryParameters: queryParameters,
-                    schema: schema,
-                    schemaProperties: schemaProperties,
-                    example: example
-                });
-            });
-        });
-
-        apiResources.push({
-            name: resource.displayName(),
-            endpoints: endpoints
-        });
-    });
-
-    // pass variables to jade template
-    var locals = {
-        ramlVersion: api.RAMLVersion(),
-        apiTitle: api.title(),
-        apiDescription: api.description() && api.description().value(),
-        apiBaseUri: api.baseUri().value().replace('{version}', api.version()),
-        apiDocumentations: apiDocumentations,
-        apiResources: apiResources
-    };
-    // console.log(JSON.stringify(locals));
+    // convert RAML to API object
+    var ramlFile = path.resolve(process.cwd(), program.file);
+    var ramlApi = api(ramlFile);
 
     // compile sass styles
     var scss = sass.renderSync({
@@ -151,16 +38,14 @@ if (program.file) {
     fs.writeFileSync(path.join(__dirname, 'src/main.css'), scss.css);
 
     // render html from jade template
-    var html = jade.renderFile(path.join(__dirname, 'src/index.jade'), locals);
+    var html = jade.renderFile(path.join(__dirname, 'src/index.jade'), ramlApi);
 
     // save html file with documentation
-    fs.writeFileSync(path.resolve(process.cwd(), 'api.html'), html);
+    fs.writeFileSync(path.resolve(process.cwd(), docFile), html);
+
+    console.timeEnd('time');
+    console.log(chalk.green('finished'));
 }
 else {
     program.outputHelp();
 }
-
-console.timeEnd('ramlo');
-
-//resources
-//nestedResources
---
+++ src/modules/api.js
@@ -0,0 +1,191 @@
+var raml = require('raml-1-parser');
+var _ = require('lodash');
+var hljs = require('highlight.js');
+
+var ramlo = {};
+
+function produceDescription(api) {
+    var description = api.description();
+
+    return description && description.value();
+}
+
+function produceDocumentations(api) {
+    var apiDocumentations = [];
+    var ramlDocumentations = api.documentation();
+
+    _.forEach(ramlDocumentations, function(documentation) {
+        var content = documentation.content();
+
+        apiDocumentations.push({
+            title: documentation.title(),
+            content: content && content.value()
+        });
+    });
+
+    return apiDocumentations;
+}
+
+function produceResources(api) {
+    var apiResources = [];
+    var ramlResources = api.resources();
+
+    _.forEach(ramlResources, function(resource) {
+        var uri = resource.completeRelativeUri();
+        var name = resource.displayName() || capitalizeFirstLetter(uri.replace('/', ''));
+
+        apiResources.push({
+            uri: uri,
+            name: name,
+            endpoints: _.flattenDeep(produceEndpoints(resource))
+        });
+    });
+
+    return apiResources;
+}
+
+function produceEndpoints(resource) {
+    var endpoints = [];
+    var ramlNestedResources = resource.resources();
+    var ramlMethods = resource.methods();
+
+    _.forEach(ramlMethods, function(method) {
+        var description = method.description();
+
+        endpoints.push({
+            uri: resource.completeRelativeUri(),
+            method: method.method(),
+            description: description && description.value(),
+            uriParameters: produceUriParameters(resource),
+            queryParameters: produceQueryParameters(method),
+            requestBody: produceRequestBody(method),
+            responseBody: produceResponseBody(method),
+            responseExample: produceResponseExample(method)
+        });
+    });
+
+    if (ramlNestedResources.length) {
+        _.forEach(ramlNestedResources, function(resource) {
+            endpoints.push(produceEndpoints(resource));
+        })
+    }
+
+    return endpoints;
+}
+
+function produceUriParameters(resource) {
+    var apiUriParameters = [];
+    var ramlUriParameters = resource.uriParameters();
+
+    _.forEach(ramlUriParameters, function(parameter) {
+        var description = parameter.description();
+
+        apiUriParameters.push({
+            name: parameter.name(),
+            type: parameter.type(),
+            description: description && description.value()
+        });
+    });
+
+    return apiUriParameters;
+}
+
+function produceQueryParameters(method) {
+    var apiQueryParameters = [];
+    var ramlQueryParameters = method.queryParameters();
+
+    _.forEach(ramlQueryParameters, function(parameter) {
+        var description = parameter.description();
+
+        apiQueryParameters.push({
+            name: parameter.name(),
+            type: parameter.type(),
+            description: description && description.value()
+        });
+    });
+
+    return apiQueryParameters;
+}
+
+function produceRequestBody(method) {
+    var apiBodySchema = [];
+    var ramlBody = method.body();
+
+    _.forEach(ramlBody, function(body) {
+        apiBodySchema = produceSchemaParameters(body.schemaContent());
+    });
+
+    return apiBodySchema;
+}
+
+function produceResponseBody(method) {
+    var ramlResponses = method.responses();
+    var ramlBodies;
+    var schemaProperties = [];
+
+    _.forEach(ramlResponses, function(response) {
+        if (response.code().value() === '200') {
+            ramlBodies = response.body();
+
+            _.forEach(ramlBodies, function(body) {
+                schemaProperties = produceSchemaParameters(body.schemaContent());
+            });
+        }
+    });
+
+    return schemaProperties;
+}
+
+function produceResponseExample(method) {
+    var apiExample = '';
+    var ramlResponses = method.responses();
+    var ramlBodies;
+
+    _.forEach(ramlResponses, function(response) {
+        if (response.code().value() === '200') {
+            ramlBodies = response.body();
+
+            _.forEach(ramlBodies, function(body) {
+                apiExample = body.toJSON().example;
+                apiExample = apiExample && hljs.highlight('json', apiExample).value;
+            });
+        }
+    });
+
+    return apiExample;
+}
+
+function produceSchemaParameters(schemaContent) {
+    var schemaObject = JSON.parse(schemaContent);
+    var schemaProperties = [];
+
+    if (_.has(schemaObject, 'properties')) {
+        _.forOwn(schemaObject.properties, function(value, key) {
+            schemaProperties.push({
+                name: key,
+                type: value.type,
+                description: value.description,
+                isRequired: value.required
+            });
+        });
+    }
+
+    return schemaProperties;
+}
+
+function capitalizeFirstLetter(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+module.exports = function(ramlFile) {
+    var api = raml.loadApiSync(ramlFile);
+
+    ramlo.ramlVersion = api.RAMLVersion();
+    ramlo.apiTitle = api.title();
+    ramlo.apiDescription = produceDescription(api);
+    ramlo.apiBaseUri = api.baseUri().value().replace('{version}', api.version());
+    ramlo.apiDocumentations = produceDocumentations(api);
+    ramlo.apiResources = produceResources(api);
+
+    return ramlo;
+};
