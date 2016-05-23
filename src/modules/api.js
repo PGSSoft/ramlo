2e6d10df54e6d7b98224ef6bc77d2883614604a2
marin metohu <marinmetohu@gmail.com>
2016-05-23 16:35:57 +0200

    merge complete

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,6 +2,7 @@ var raml = require('raml-1-parser');
 var _ = require('lodash');
 var hljs = require('highlight.js');
 var chalk = require('chalk');
+var markdown = require('markdown').markdown;
 
 var ramlo = {};
 
@@ -15,7 +16,7 @@ function produceDocumentations(api) {
     var apiDocumentations = [];
     var ramlDocumentations = api.documentation();
 
-    _.forEach(ramlDocumentations, function(documentation) {
+    _.forEach(ramlDocumentations, function (documentation) {
         var content = documentation.content();
 
         apiDocumentations.push({
@@ -31,7 +32,7 @@ function produceResources(api) {
     var apiResources = [];
     var ramlResources = api.resources();
 
-    _.forEach(ramlResources, function(resource) {
+    _.forEach(ramlResources, function (resource) {
         var uri = resource.completeRelativeUri();
         var name = resource.displayName() || capitalizeFirstLetter(uri.replace('/', ''));
 
@@ -50,14 +51,14 @@ function produceEndpoints(resource) {
     var endpoints = [];
     var ramlNestedResources = resource.resources();
     var ramlMethods = resource.methods();
-    
-    _.forEach(ramlMethods, function(method) {
-        var description = method.description();
+
+    _.forEach(ramlMethods, function (method) {
+        var description = method.description() && markdown.toHTML(method.description().value());
 
         endpoints.push({
             uri: resource.completeRelativeUri(),
             method: method.method(),
-            description: description && description.value(),
+            description: description,
             uriParameters: produceUriParameters(resource),
             queryParameters: produceQueryParameters(method),
             requestBody: produceRequestBody(method),
@@ -67,7 +68,7 @@ function produceEndpoints(resource) {
     });
 
     if (ramlNestedResources.length) {
-        _.forEach(ramlNestedResources, function(resource) {
+        _.forEach(ramlNestedResources, function (resource) {
             endpoints.push(produceEndpoints(resource));
         })
     }
@@ -81,30 +82,27 @@ function produceUriParameters(resource) {
         thead: {
             name: false,
             type: false,
-            description: false,
+            description: false
         },
-        tbody:[]
+        tbody: []
     };
     var ramlUriParameters = resource.uriParameters();
 
-    _.forEach(ramlUriParameters, function(parameter) {
-
-        //console.log("*", Object.keys( parameter ).length );
-
+    _.forEach(ramlUriParameters, function (parameter) {
         var description = parameter.description();
 
         //check if type exists
-        if(apiUriParameters.thead.type == false && parameter.type() != null){
+        if (apiUriParameters.thead.type == false && parameter.type() != null) {
             apiUriParameters.thead.type = true;
         }
 
         //check if description exists
-        if(apiUriParameters.thead.description == false && description != null){
+        if (apiUriParameters.thead.description == false && description != null) {
             apiUriParameters.thead.description = true;
         }
 
         //check if example exists
-        if(apiUriParameters.thead.name == false && parameter.name() != null){
+        if (apiUriParameters.thead.name == false && parameter.name() != null) {
             apiUriParameters.thead.name = true;
         }
 
@@ -130,59 +128,60 @@ function produceQueryParameters(method) {
             minLength: false,
             maxLength: false
         },
-        tbody:[]
+        tbody: []
     };
     var ramlQueryParameters = method.queryParameters();
 
-    _.forEach(ramlQueryParameters, function(parameter) {
-
+    _.forEach(ramlQueryParameters, function (parameter) {
         var description = parameter.description();
-        var minLength  = "";
-        var maxLength  = "";
+        var minLength = "";
+        var maxLength = "";
 
         //check if name exists
-        if(apiQueryParameters.thead.name == false && parameter.name() != null){
+        if (apiQueryParameters.thead.name == false && parameter.name() != null) {
             apiQueryParameters.thead.name = true;
         }
 
         //check if type exists
-        if(apiQueryParameters.thead.type == false && parameter.type() != null){
+        if (apiQueryParameters.thead.type == false && parameter.type() != null) {
             apiQueryParameters.thead.type = true;
         }
 
         //check if description exists
-        if(apiQueryParameters.thead.description == false && description != null){
+        if (apiQueryParameters.thead.description == false && description != null) {
             apiQueryParameters.thead.description = true;
         }
 
         //check if example exists
-        if(apiQueryParameters.thead.example == false && parameter.example() != null){
+        if (apiQueryParameters.thead.example == false && parameter.example() != null) {
             apiQueryParameters.thead.example = true;
         }
 
         //check if default exists
-        if(apiQueryParameters.thead.default == false && parameter.default() != null){
+        if (apiQueryParameters.thead.default == false && parameter.default() != null) {
             apiQueryParameters.thead.default = true;
         }
 
-        try{
+        try {
             /* note: parameter.minLength() & parameter.maxLength() throws an error if no minLength | maxLength exists
-                    it is an error from the parser, try...catch solves the problem
+             it is an error from the parser, try...catch solves the problem
              */
-            if(apiQueryParameters.thead.minLength == false && parameter.minLength() != null){
+            if (apiQueryParameters.thead.minLength == false && parameter.minLength() != null) {
                 apiQueryParameters.thead.minLength = true;
                 minLength = parameter.minLength();
             }
         }
-        catch(err){}
+        catch (err) {
+        }
 
-        try{
-            if(apiQueryParameters.thead.maxLength == false && parameter.maxLength() != null){
+        try {
+            if (apiQueryParameters.thead.maxLength == false && parameter.maxLength() != null) {
                 apiQueryParameters.thead.maxLength = true;
                 maxLength = parameter.maxLength();
             }
         }
-        catch(err){}
+        catch (err) {
+        }
 
 
         apiQueryParameters["tbody"].push({
@@ -203,25 +202,24 @@ function produceQueryParameters(method) {
 
 function produceRequestBody(method) {
     var apiBodySchema = {
-        thead: {
-        },
+        thead: {},
         tbody: []
     };
 
     var ramlBody = method.body();
 
-    if(Object.keys(ramlBody).length > 0){
+    if (Object.keys(ramlBody).length > 0) {
 
         //expecting only 1 value to be valid
         //there should NOT be 2 or more "body" declarations
 
         _.forEach(ramlBody, function (body) {
 
-            if(body.schemaContent() != null ){
+            if (body.schemaContent() != null) {
                 var sp = produceSchemaParameters(body.schemaContent());
 
                 //make sure that "body" key was valid
-                if(sp["tbody"].length > 0 ){
+                if (sp["tbody"].length > 0) {
                     apiBodySchema = sp;
                 }
 
@@ -238,21 +236,21 @@ function produceResponseBody(method) {
     var ramlBodies;
     var schemaProperties = [];
 
-    _.forEach(ramlResponses, function(response) {
+    _.forEach(ramlResponses, function (response) {
 
-        if (response.code().value() === '200' ) {
+        if (response.code().value() === '200') {
             ramlBodies = response.body();
 
-            _.forEach(ramlBodies, function(body) {
+            _.forEach(ramlBodies, function (body) {
 
                 //check if NULL before calling produceSchemaParameters()
                 var sch = body.schemaContent();
 
-                if(sch != null && typeof sch != "undefined"){
+                if (sch != null && typeof sch != "undefined") {
 
-                    var sp = produceSchemaParameters( sch );
+                    var sp = produceSchemaParameters(sch);
 
-                    if(sp["tbody"].length > 0){
+                    if (sp["tbody"].length > 0) {
                         schemaProperties.push(sp);
                     }
                 }
@@ -302,13 +300,12 @@ function produceSchemaParameters(schemaContent) {
             type: false,
             description: false
         },
-        tbody:[]
+        tbody: []
     };
-
 
     //before calling JSON.parse, make sure the string is valid json
     //using try/catch solved errors, ex. https://github.com/raml-apis/Instagram
-    try{
+    try {
         var schemaObject = _.isObject(schemaContent) ? schemaContent : JSON.parse(schemaContent);
         var nestedProperties;
 
@@ -317,7 +314,7 @@ function produceSchemaParameters(schemaContent) {
         }
 
         if (_.has(schemaObject, 'properties')) {
-            _.forOwn(schemaObject.properties, function(value, key) {
+            _.forOwn(schemaObject.properties, function (value, key) {
                 nestedProperties = [];
 
                 if (_.has(value, 'items')) {
@@ -329,27 +326,27 @@ function produceSchemaParameters(schemaContent) {
                 }
 
                 //check if description exists
-                if(schemaProperties.thead.description == false && value.description != null){
+                if (schemaProperties.thead.description == false && value.description != null) {
                     schemaProperties.thead.description = true;
                 }
 
-                if(schemaProperties.thead.type == false && value.type != null){
+                if (schemaProperties.thead.type == false && value.type != null) {
                     schemaProperties.thead.type = true;
                 }
 
                 schemaProperties["tbody"].push({
                     name: key,
                     type: value.type,
-                    description:  value.description,
+                    description: value.description,
                     isRequired: value.required,
                     nestedProperties: nestedProperties
                 });
             });
         }
     }
-    catch(err){
-        console.log( err ,  schemaContent);
-        console.log( "////////////////////////////////////////////////////" );
+    catch (err) {
+        console.log(err, schemaContent);
+        console.log("////////////////////////////////////////////////////");
     }
 
     return schemaProperties;
@@ -359,25 +356,25 @@ function capitalizeFirstLetter(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
 
-module.exports = function(ramlFile) {
+module.exports = function (ramlFile) {
     var api;
     var apiBaseUri = "";
 
     try {
         api = raml.loadApiSync(ramlFile).expand(); //expand() fixed the problem with traits
     }
-    catch(e) {
+    catch (e) {
         console.log(chalk.red('provided file is not a correct RAML file!'));
         process.exit(1);
     }
 
-    try{
+    try {
         apiBaseUri = api.baseUri().value().replace('{version}', api.version());
     }
-    catch(err){
+    catch (err) {
         console.log("BaseUri" + err);
     }
-    
+
     ramlo.ramlVersion = api.RAMLVersion();
     ramlo.apiTitle = api.title();
     ramlo.apiDescription = produceDescription(api);
