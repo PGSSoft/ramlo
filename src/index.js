8b9f6701e9a3567cfed0e45735d95c6127548c2e
arichter <arichter@pgs-soft.com>
2016-06-24 16:06:10 +0200

    Added variant of examples expanding, moved endpoints documentation styles to separate scss

--- src/index.js
+++ src/index.js
@@ -77,11 +77,37 @@
         });
     }
 
+    function initExpandableExamples() {
+        $('.documentation__endpoints__examples a.examples__toggle').click(function () {
+            toggleExpandExamples($(this).parent());
+        });
+    }
+
+    var EXPAND_TIMEOUT_MS = 500;
+
+    function toggleExpandExamples(example) {
+        var isExpanded = example.hasClass('is-expanded');
+        if (example) {
+            if (isExpanded) {
+                example.addClass('is-collapsing').removeClass('is-expanded'); //.children('.documentation__endpoints__examples')
+                setTimeout(function () {
+                    example.parent().height('');
+                    example.removeClass('is-collapsing');
+                }, EXPAND_TIMEOUT_MS);
+            } else {
+                var endpointHeight = example.outerHeight();
+                example.parent().height(endpointHeight);
+                example.addClass('is-expanded'); //.children('.documentation__endpoints__examples')
+            }
+        }
+    }
+
     //initialize scripts
     $(document).ready(function () {
         stickySidebar();
         animateScroll();
         initTabs();
+        initExpandableExamples();
     });
 
 })($);
