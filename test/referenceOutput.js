97ed3d4cef514f33cf18df332a981d48fea6f43a
arichter <arichter@pgs-soft.com>
2016-07-29 17:34:04 +0200

    Extended types definitions, included data types in main raml file, added tests & comments

--- test/referenceOutput.js
+++ test/referenceOutput.js
@@ -1,12 +1,36 @@
+/*
+ This is the reference test output of ramlo - the main object is grouped by several main properties
+ into sections covering the RAML Version 1.0 specification.
+ The sections are:
+ -basicInfo
+ -dataTypes
+ -resources
+ -methods
+ -responses
+ -annotations
+ */
 module.exports = {
-    ramlVersion: 'RAML10',
-    apiTitle: 'Example API',
-    apiDescription: '',
-    apiVersion: 'v2',
-    apiBaseUri: '',
-    baseUriParameters: [],
-    apiProtocol: 'Protocols: HTTP, HTTPS',
-    apiSecuredBy: {},
-    apiSecuritySchemes: []
-    //mediaType: 'application/json'
+    basicInfo: {
+        ramlVersion: 'RAML10',
+        apiTitle: 'Example API',
+        apiDescription: '',
+        apiVersion: 'v2',
+        apiBaseUri: '',
+        baseUriParameters: [],
+        apiProtocol: 'Protocols: HTTP, HTTPS',
+        apiSecuredBy: {},
+        apiSecuritySchemes: []
+    },
+    //mediaType: ['application/json'],
+    dataTypes: {
+        apiAllTypes: [],
+        typeNamesArray: []
+    },
+    resources: {
+        apiResources: []
+    },
+    methods: {},
+    responses: {},
+    annotations: {}
+
 };
--- test/test.js
+++ test/test.js
@@ -10,44 +10,83 @@ var test = 'test/spec/api.raml';
 var referenceRamlo = require('./referenceOutput');
 
 //make sure the path of the file is correct before running the test
-//module.exports = function () {
 if (fs.existsSync(test)) {
-    describe('API basic information', function () {
+
+    describe('Ramlo', function () {
 
         var rm = ramloApi(test);
 
-        it('Ramlo should return an object', function () {
-            expect(typeof rm).to.equal('object');
+        describe('API basic information', function () {
+
+            it('Ramlo should return an object', function () {
+                expect(typeof rm).to.equal('object');
+            });
+
+            //Checking all of the basic properties
+            _.forOwn(referenceRamlo.basicInfo, function (refVal, refKey) {
+                // Each property has its own test suite
+                it('Should have valid property ' + refKey, function () {
+
+                    //#1 - does the property exist
+                    expect(rm).to.have.ownProperty(refKey);
+                    var prop = rm[refKey];
+                    var type = _.isArray(refVal) ? 'array' : typeof refVal;
+
+                    //#2 - is the property of proper type
+                    expect(prop).to.be.a(type);
+
+                    //#3 - is the property value equal to reference (skipped if reference is null/undefined/empty)
+                    if (!(_.isNil(refVal) || _.isEmpty(refVal))) {
+                        if (_.isArray(refVal)) {
+                            expect(prop.length).to.equal(refVal.length);
+                            //TODO tests to compare array contents
+                        } else {
+                            expect(prop).to.equal(refVal);
+                        }
+                    }
+
+                });
+            });
+
         });
 
-        _.forOwn(referenceRamlo, function (val, key) {
-            // Each property has its own test suite
-            it('Should have valid property ' + key, function () {
+        // Checking data types with example type 'Person' and extending types
+        describe('API Data Types', function () {
+            var foundType = null;
+            it('Should contain data types array', function () {
+                expect(rm).to.have.ownProperty('apiAllTypes');
+                expect(rm.apiAllTypes).to.be.a('array');
+            });
 
-                //#1 - does the property exist
-                expect(rm).to.have.ownProperty(key);
-                var prop = rm[key];
-                var type = _.isArray(val) ? 'array' : typeof val;
+            it('Data types should contain Person type object', function () {
+                foundType = _.find(rm.apiAllTypes, 'Person');
+                expect(foundType).to.be.a('object');
+            });
 
-                //#2 - is the property of proper type
-                expect(prop).to.be.a(type);
-
-                //#3 - is the property value equal to reference (skipped if reference is empty)
-                if (!(_.isNil(val) || _.isEmpty(val)))
-                    expect(prop).to.equal(val);
+            it('Person type should contain valid properties', function () {
+                var props = _.get(foundType, 'Person.properties');
+                expect(props).to.be.a('object');
+                expect(props).to.contain.all.keys(['firstname', 'lastname', 'title']);
+            });
+            it('Person type should contain example', function () {
+                var example = _.get(foundType, 'Person.example');
+                expect(example).to.be.a('object');
+                expect(example).to.contain.all.keys(['firstname', 'lastname']);
             });
         });
 
-        //describe('check prop values', function () {
-        //    it('apiDescription should be formatted in markdown', function () {
-        //        expect(rm.apiDescription).to.contain('<p>').and.contain('</p>');
-        //    });
-        //});
+        //Checking resources
+        describe('API Resources', function () {
+
+        });
+
+        //Checking documentations including endpoints
+        describe('API Documentations', function () {
+
+        });
 
     });
-
 }
 else {
     console.log("the file doesn't exist");
 }
-//};
